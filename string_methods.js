4f61fb3e9636e745e4cfe0f266ec56137541221b
Nikita Kravchuk <nikita.kravchuk@ukr.net>
2021-07-29 22:51:28 +0300

    commit

--- string_methods.js
+++ string_methods.js
@@ -1,20 +1,23 @@
 var str = "",
   inclStr = "",
   i, 
+  j,
   start = 0;
 
-function includes(str, inclStr, start) {
+function includes(str, inclStr) {
   str = str.toLowerCase().split('');
   inclStr = inclStr.toLowerCase().split('');
   console.log(str, inclStr);
 
-  for (i = 0 + start; i < inclStr.length + start; i += 1) {
-    if (str[i] === inclStr[i]) {
-      console.log("есть элемент в строке" + " " +`${str[i]}`);
-    } else {
-      console.log("элемента нет");
+  for (i = 0; i < inclStr.length; i += 1) {
+    for(j = 0; j <  str.length; j += 1){
+      if (inclStr[i] === str[j]) {
+        console.log("есть элемент в строке " +`${str[j]}`);
+      } else {
+        console.log("элемента нет");
+      }
     }
   }
 }
 
-includes("Hello world", "world", 6);
+includes("Hello world", "world");
