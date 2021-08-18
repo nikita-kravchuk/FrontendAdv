a39898860457dabafd95f1e0f9cffc31078b8e75
Nikita Kravchuk <nikita.kravchuk@ukr.net>
2021-08-18 23:35:31 +0300

    commit

---
+++ freeCodeCamp/romanNumeralConv.js
@@ -0,0 +1,33 @@
+// Roman Numeral Converter
+// Convert the given number into a roman numeral.
+
+// All roman numerals answers should be provided in upper-case.
+
+function convertToRoman(num) {
+  var romanNum = {
+      M: 1000,
+      CM: 900,
+      D: 500,
+      CD: 400,
+      C: 100,
+      XC: 90,
+      L: 50,
+      XL: 40,
+      X: 10,
+      IX: 9,
+      V: 5,
+      IV: 4,
+      I: 1,
+    },
+    roman = "",
+    i;
+  for (i in romanNum) {
+    while (num >= romanNum[i]) {
+      roman += i;
+      num -= romanNum[i];
+    }
+  }
+  return roman;
+}
+
+convertToRoman(36);
